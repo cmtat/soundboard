35c534c35838eb162d414e7de21fe7e449dbd71f
cmtat <carter.m.tatum@gmail.com>
2025-11-25 19:01:53 +0000

    appwrite v2

--- appwrite.js
+++ appwrite.js
@@ -1,7 +1,7 @@
-import { Client, Account, Databases } from "appwrite";
+// Import Appwrite from a CDN so it works in the browser without a bundler.
+import { Client, Account, Databases } from "https://esm.run/appwrite@21.4.0";
 
 const client = new Client().setEndpoint("https://nyc.cloud.appwrite.io/v1").setProject("soundboard");
-
 const account = new Account(client);
 const databases = new Databases(client);
 
